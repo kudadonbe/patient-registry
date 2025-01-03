b1bf12da751bbb9d85d3127ac5a9ba3b6b6e8a0e
Hussain Shareef <hussain.shareef@gmail.com>
2025-01-04 01:56:31 +0500

    Update Firebase configuration and dependencies; add env.js to .gitignore

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,18 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBaxQLbFJtGLG5kY6A7OIVbfVdsi7sV2Vc",
-  authDomain: "neeli-bd015.firebaseapp.com",
-  projectId: "neeli-bd015",
-  storageBucket: "neeli-bd015.firebasestorage.app",
-  messagingSenderId: "793849375428",
-  appId: "1:793849375428:web:0f0bb54c38e5f871f1b791"
-};
+import { firebaseConfig } from "@/firebase/env";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
